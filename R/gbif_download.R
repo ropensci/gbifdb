46b2cb4fb0b180a767c8f091d00533d024d3ea3c
Carl <cboettig@gmail.com>
2021-11-30 19:04:13 +0000

    :dolphin:

--- R/gbif_download.R
+++ R/gbif_download.R
@@ -1,6 +1,9 @@
+## Developer NOTE: consider switchng to arrow::S3FileSystem-based method.
+## doesn't support sync out-of-box, but lighter dependency since arrow is already in use.
+
 
 #' Download GBIF data using aws.s3 sync
-#' 
+#'
 #' Sync a local directory with selected release of the AWS copy of GBIF
 #' @param version 'prefix' string (folder) from the 
 #' https://registry.opendata.aws/gbif/ which should be synced.
@@ -8,24 +11,24 @@
 #' @details
 #' Sync parquet files from GBIF public data catalogue,
 #' https://registry.opendata.aws/gbif/
-#' 
+#'
 #' Note that data can also be found on the Microsoft Cloud,
 #' https://planetarycomputer.microsoft.com/dataset/gbif
-#' 
+#'
 #' Also, some users may prefer to download this data using an alternative
 #' interface or work on a cloud-host machine where data is already available.
 #' @export
-#' 
+#'
 #' @examplesIf interactive()
 #' gbif_download()
-#' 
+#'
 gbif_download <-
   function(version = "2021-11-01",
            dir = gbif_dir()
            ){
 
   ## Fixme detect version, maybe w/o AWS dependency
-  
+
   if(!requireNamespace("aws.s3", quietly = TRUE)){
     message("the aws.s3 package is required for automatic download")
     return(invisible(NULL))
@@ -41,13 +44,15 @@ gbif_download <-
 
 
 #' Default storage location
-#' 
+#'
 #' Default location can be set with the env var GBIF_HOME,
 #' otherwise will use the default provided by [tools::R_user_dir()]
-#' 
+#'
 #' @export
 gbif_dir <- function(){
   Sys.getenv("GBIF_HOME",
              tools::R_user_dir("gbif"))
-             
+
 }
+
+
